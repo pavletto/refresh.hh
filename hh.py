0a78555d6ff8f19005e4f15b3b6d4a125000cffb
Pavel Sukhodolskii <pvlt.job@gmail.com>
2016-01-14 17:24:16 +0300

    added the log entry

--- hh.py
+++ hh.py
@@ -2,14 +2,14 @@
 import requests
 import logging
 
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
 resume_id = ''
 access_token = ''
+LOG_FILENAME = 'hh.log'
+logging.getLogger("requests.packages.urllib3")
+logging.propagate = True
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG, format=u'%(levelname)-8s [%(asctime)s]  %(message)s')
+
 url = 'https://api.hh.ru/resumes/'+resume_id+'/publish'
-headers ={'User-Agent': 'pvlt(pvlt.job@gmail.com)', 'Authorization': 'Bearer '+access_token}
+headers ={'Authorization': 'Bearer '+access_token}
 
 r = requests.post(url, headers=headers)
